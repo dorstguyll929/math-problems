854149398cd85f44a7b20f79b70862a965a8f265
dorstguyll929 <dorstguyll929@gmail.com>
2025-03-06 08:51:21 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,11 @@
+
+Here is some example code for you: 
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2;
+    printf("Enter two numbers to add:\n");
+    scanf("%d %d", &num1, &num2);
+    printf("The sum of the two numbers is: %d\n", num1 + num2);
+    return 0;
+}
