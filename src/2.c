17b1abe6ef4c92611936741477047045eb93d609
dorstguyll929 <dorstguyll929@gmail.com>
2025-03-07 12:24:01 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    double y = 2.5;
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %lf\n", y);
+    return 0;
+}
