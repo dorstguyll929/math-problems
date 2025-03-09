f3cfb7afb76a8a3750b31e5c064ab163bf3471c1
dorstguyll929 <dorstguyll929@gmail.com>
2025-03-09 04:25:33 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,19 @@
+// Return the sum of two numbers
+int add(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Return the difference of two numbers
+int subtract(int num1, int num2) {
+  return num1 - num2;
+}
+
+// Return the product of two numbers
+int multiply(int num1, int num2) {
+  return num1 * num2;
+}
+
+// Return the quotient of two numbers
+int divide(int num1, int num2) {
+  return num1 / num2;
+}
