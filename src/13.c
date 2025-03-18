d01c36c04b33cf79378276a07320858b78954c06
dorstguyll929 <dorstguyll929@gmail.com>
2025-03-18 14:07:03 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int x = 5;
+    float y = 3.14;
+    double z = 2.71828;
+
+    printf("The sum of x and y is %f\n", x + y);
+    printf("The product of x and y is %f\n", x * y);
+    printf("The square root of x is %f\n", sqrt(x));
+    printf("The cube root of z is %f\n", cbrt(z));
+
+    return 0;
+}
