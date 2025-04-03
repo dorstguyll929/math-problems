0b9ebc554f3747b0a823bd1d54fd7202978c699f
dorstguyll929 <dorstguyll929@gmail.com>
2025-04-03 12:47:56 +0000

    Add 23.c

---
+++ src/23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %.2f\n", (float)a / b);
+
+    return 0;
+}
