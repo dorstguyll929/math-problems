7e1b71d06a61410c7895f4a4f311ae7e19af34d8
dorstguyll929 <dorstguyll929@gmail.com>
2025-04-11 11:08:03 +0000

    Add 29.c

---
+++ src/29.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &i);
+
+    for (i = 1; i <= i; ++i) {
+        if (i % 2 == 0) {
+            printf("*");
+        } else {
+            printf("#");
+        }
+    }
+
+    return 0;
+}
